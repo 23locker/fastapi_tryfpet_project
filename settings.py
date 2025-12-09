19cf01cbc3d4784fba8933476f281e119f991ced
tt <1tokatoma1@gmail.com>
2025-12-09 16:30:52 +0300

    add to gitignore uv.lock and configure settings.py for db

---
+++ settings.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
+
+REAL_DATABASE_URL = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+
+print(settings.DB_HOST)

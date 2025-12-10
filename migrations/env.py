d1a51ccbfbc93e3d65694f073e8fc1b766ec3a96
tt <1tokatoma1@gmail.com>
2025-12-10 16:27:47 +0300

    update migrations env and tweak pyproject config

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from os.path import abspath, dirname
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from settings import settings
+from config import settings
 
 # need for file alembic
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))

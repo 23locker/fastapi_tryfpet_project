ab1ddf4ef075177c991b7dd4d780b696d07c533e
tt <1tokatoma1@gmail.com>
2025-12-11 22:02:52 +0300

    create user repository

---
+++ app/repositories/user.py
@@ -0,0 +1,59 @@
+from typing import Optional
+from uuid import UUID
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.user import User
+from app.repositories.base import BaseRepository
+
+
+class UserRepository(BaseRepository[User]):
+    """Repository for work with users"""
+
+    def __init__(self, session: AsyncSession):
+        super().__init__(session, User)
+
+    async def get_by_email(self, email: str) -> Optional[User]:
+        """
+        Receives the user by email
+
+        args:
+            email: user email
+        """
+        result = await self.session.execute(select(User).where(User.email == email))
+        return result.scalars().first()
+
+    async def get_user_by_id(self, user_id: UUID) -> Optional[User]:
+        """
+        Take user by UUID
+
+        args:
+            user_id: UUID user
+        """
+        result = await self.session.execute(select(User).where(User.user_id == user_id))
+        return result.scalars().first()
+
+    async def get_activate_users(self, skip: int = 0, limit: int = 10) -> list[User]:
+        """
+        Get only active users
+
+        args:
+            skip: number of entries to skip
+            limit: maximum number of entires
+        """
+        result = await self.session.execute(
+            select(User).where(User.is_active == True).offset(skip).limit(limit)
+        )
+        return result.scalars().all()
+
+    async def user_exists(self, email: str) -> bool:
+        """
+        Check what user exists with that email
+        """
+        result = await self.session.execute(
+            select(User).where(User.email == email).limit(1)
+        )
+        return result.scalars().first() is not None
+        
+    
